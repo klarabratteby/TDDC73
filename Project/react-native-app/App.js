5e70cc8cc239635acbbddf000e223b249294a969
Clara Lennartsson <clara.lennartsson99@gmail.com>
2024-12-16 10:50:36 +0100

    Implemented App.js

--- Project/react-native-app/App.js
+++ Project/react-native-app/App.js
@@ -1,10 +1,57 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { React, useState } from "react";
+import Carousel from "./components/Carousel";
+import Password from "./components/Password";
 
 export default function App() {
+  //example data for carousel
+  const data = [
+    { uri: "https://via.placeholder.com/300/FF6347" },
+    { uri: "https://via.placeholder.com/300/4682B4" },
+    { uri: "https://via.placeholder.com/300/FFD700" },
+    { uri: "https://via.placeholder.com/300/32CD32" },
+    { uri: "https://via.placeholder.com/300/FF4500" },
+    { uri: "https://via.placeholder.com/300/8A2BE2" },
+    //{ uri: "https://via.placeholder.com/300/D2691E" },
+    //{ uri: "https://via.placeholder.com/300/DC143C" },
+    //{ uri: "https://via.placeholder.com/300/4B0082" },
+    //{ uri: "https://via.placeholder.com/300/9932CC" },
+  ];
+  // password criterias
+  const [password, setPassword] = useState("");
+
+  const passwordSettings = {
+    criteria: {
+      minLength: 8,
+      weakLength: 10,
+      fairLength: 12,
+      goodLength: 14,
+      hasLowerCase: /[a-z]/,
+      hasUpperCase: /[A-Z]/,
+      hasNumber: /\d/,
+      hasSpecialChar: /[!@#$%^&*(),.?":{}|<>]/,
+    },
+    colors: {
+      tooShort: "#8C8C8C",
+      weak: "#8C0000",
+      fair: "#E9C930",
+      good: "#5C9DC9",
+      strong: "#21772E",
+    },
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Our SDK App</Text>
+      <Text style={styles.label}>Carousel component</Text>
+      <Carousel images={data} numVisible={1} />
+      <Text style={styles.label}>Password component</Text>
+      <Password
+        passwordSettings={passwordSettings}
+        onPasswordChange={setPassword}
+        password={password}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +63,15 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    gap: 20,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    textDecorationLine: "underline",
+  },
+  label: {
+    fontSize: 24,
+    fontWeight: "bold",
   },
 });
